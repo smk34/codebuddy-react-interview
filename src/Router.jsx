e8edf72124c996b30f303481fdd7a1aecdf6f88e
Soumik Sarkar <20475126+smk34@users.noreply.github.com>
2022-01-25 12:58:45 +0530

    Change updated

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import Fallback from './Fallback';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const Posts = React.lazy(() => import('./pages/Posts'));
+const BookSeat = React.lazy(() => import('./pages/BookSeat'));
 
 const Router = () => (
   <React.Suspense fallback={<Fallback />}>
@@ -11,6 +12,7 @@ const Router = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Posts} />
+        <Route exact path="/bookSeat" component={BookSeat} />
       </Switch>
     </BrowserRouter>
   </React.Suspense>
